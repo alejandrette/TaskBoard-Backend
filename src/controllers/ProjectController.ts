6a286e9d51054ae731047d7a5a1fa37f3708f042
Alejandrette <alejandrette5117@gmail.com>
2025-04-11 13:09:08 +0200

    create enpoint post projects

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,7 +1,18 @@
 import type { Request, Response } from "express";
+import Project from "../models/Project";
 
 export class ProjectController {
   static getAllProjects = async (req: Request, res: Response) => {
     res.send('All projects')
+  }
+
+  static createProject = async (req: Request, res: Response) => {
+    const project = new Project(req.body)
+    try {
+      await project.save()
+      res.send('Project created successfull')
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
