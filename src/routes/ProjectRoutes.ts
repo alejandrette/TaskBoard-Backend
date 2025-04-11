f23aad1d29095a73ec4aac182a024bc839e2e97d
Alejandrette <alejandrette5117@gmail.com>
2025-04-11 13:29:58 +0200

    create middleware

--- src/routes/ProjectRoutes.ts
+++ src/routes/ProjectRoutes.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
 router.get('/', ProjectController.getAllProjects)
-router.post('/', ProjectController.createProject)
+router.post('/',
+  body('projectName')
+    .notEmpty().withMessage('The project name is empty'),
+  body('clientName')
+    .notEmpty().withMessage('The client name is empty'),
+  body('description')
+    .notEmpty().withMessage('The description is empty'),
+  handleInputErrors,
+  ProjectController.createProject
+)
 
 export default router
