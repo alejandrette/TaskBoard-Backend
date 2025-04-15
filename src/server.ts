3a29340bf96a9c310cf087c48ba75a6005389714
Alejandrette <alejandrette5117@gmail.com>
2025-04-15 17:32:05 +0200

    create config cors

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import express from 'express'
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/ProjectRoutes'
+import { corsConfig } from './config/cors';
+import cors from 'cors';
 
 dotenv.config()
 connectDB()
 
 const app = express()
+app.use(cors(corsConfig))
 app.use(express.json())
 
 //Router
