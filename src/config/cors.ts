1bb53381bec49b0a59af4d6d440f6e299043feea
Alejandrette <alejandrette5117@gmail.com>
2025-04-16 19:50:43 +0200

    config cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,13 +1,13 @@
 import { CorsOptions } from "cors"
 
 export const corsConfig: CorsOptions = {
-  origin: function(origin, callback){
+  origin: function(origin, callback) {
     const whiteList = [process.env.FRONT_URL]
 
-    if(whiteList.includes(origin)){
+    if (!origin || whiteList.includes(origin)) {
       callback(null, true)
     } else {
-      callback(new Error('Error Cors'))
+      callback(new Error('Error CORS'))
     }
   }
 }
