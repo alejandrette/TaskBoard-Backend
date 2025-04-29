a75f5ea952c813241a66875c99425db3c2b14d47
Alejandrette <alejandrette5117@gmail.com>
2025-04-29 13:46:58 +0200

    change route email

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -18,7 +18,7 @@ export class AuthEmail {
           <h2 style="color: #4F46E5;">Hello, ${name}!</h2>
           <p>Thank you for registering at <strong>TaskBoard</strong>.</p>
           <p>To activate your account, simply click the link below and enter the token: <strong style="color: #4F46E5;">${token}</strong></p>
-          <a href="#" style="display: inline-block; margin-top: 20px; background-color: #4F46E5; color: white; padding: 10px 20px; border-radius: 5px; text-decoration: none;">
+          <a href="${process.env.FRONT_URL}/auth/confirm-account" style="display: inline-block; margin-top: 20px; background-color: #4F46E5; color: white; padding: 10px 20px; border-radius: 5px; text-decoration: none;">
             Confirm Account
           </a>
           <p style="margin-top: 20px;">If you did not create this account, you can safely ignore this message.</p>
