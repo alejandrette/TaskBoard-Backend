6f5282c066a5725b893cf0968764e1e4504a486d
Alejandrette <alejandrette5117@gmail.com>
2025-06-09 17:33:23 +0200

    fix

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from "cors"
 
 export const corsConfig: CorsOptions = {
   origin: function(origin, callback) {
-    const whiteList = [process.env.FRONT_URL || 'http://localhost:5173']
+    const whiteList = [process.env.FRONT_URL]
 
     if (!origin || whiteList.includes(origin)) {
       callback(null, true)
